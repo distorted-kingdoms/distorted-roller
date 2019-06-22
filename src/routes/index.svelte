2d71c0c73c7d577819633a6773141846aa2fac9b
SonarBeserk <sonarbeserk@gmail.com>
2019-06-22 15:46:26 -0400

    Add sapper app with basic roller

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>Distorted Roller</title>
+</svelte:head>
+
+<script>
+	import EggRoller from '../components/EggRoller.svelte';
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1><center>Distorted Kingdoms Egg Roller</center></h1>
+
+<EggRoller/>
